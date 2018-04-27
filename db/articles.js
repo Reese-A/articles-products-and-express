26c1203e80fdc2c712c89c7d54f453e157654f69
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-27 13:58:54 -1000

    article setup

--- db/articles.js
+++ db/articles.js
@@ -1,17 +1,30 @@
-const collection = [];
+const collection = [{
+    title: 'place',
+    author: 'holder',
+    body: 'placeholder',
+  },
+  {
+    title: 'test',
+    author: 'testtest',
+    body: 'testtesttest'
+  }
+];
 
 function all() {
   return collection;
 }
 
 function add(res, req) {
-  // 
+
+}
+
+function getByTitle(){
 
 }
 
 module.exports = {
   all: all,
   add: add,
-  // getByTitle: getByTitle,
-  // editByTitle: editByTitle
+  getByTitle: getByTitle,
+
 };
--- routes/articles/index.js
+++ routes/articles/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const exphbs = require('express-handlebars');
-const app = express();
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+// const exphbs = require('express-handlebars');
+// const app = express();
+// app.engine('.hbs', exphbs({ extname: '.hbs' }));
+// app.set('view engine', '.hbs');
+const checkData = require('../../util/checkData');
 const articleDb = require('../../db/articles');
 
+const articles = articleDb.all()
+
+router.use(checkData());
+
+
 router.route('/')
-  .get((res, req) => {
-    res.send(articleDb.all(res, req));
+  .get((req, res) => {
+    res.render('articlesList', {
+      articles: articles
+    })
   })
 
 module.exports = router;
