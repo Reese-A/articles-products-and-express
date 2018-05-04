31bf04953791d4d93d487575980cd29ed818c682
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-04 13:41:36 -1000

    initial commit server setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'app'
+}));
+app.set('view engine', '.hbs');
+
+app.use(express.static('/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use('/', routes);
+
+app.listen(PORT, (err) => {
+  console.log(`Server running on port: ${PORT}`)
+});
